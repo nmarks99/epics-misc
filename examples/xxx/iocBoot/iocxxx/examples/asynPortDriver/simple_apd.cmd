3223acfc489eaf9c269ae92d1eb5d0a6fd116bd0
nmarks99 <nmarks@anl.gov>
2024-07-01 15:42:22 -0500

    fix cmd

--- examples/xxx/iocBoot/iocxxx/examples/asynPortDriver/simple_apd.cmd
+++ examples/xxx/iocBoot/iocxxx/examples/asynPortDriver/simple_apd.cmd
@@ -3,5 +3,4 @@ epicsEnvSet("LUA_SCRIPT_PATH", "$(LUA_SCRIPT_PATH):examples/asynPortDriver")
 SimpleApdConfig("apd_port", "device_port_name")
 dbLoadRecords("$(SIMPLE_APD)/db/apd.db", "P=$(PREFIX), PORT=apd_port, ADDR=0")
 
-# dbLoadRecords("examples/asynPortDriver/process_chain.db", "P=$(PREFIX), PORT=apd_port, ADDR=0")
-dbLoadRecords("examples/asynPortDriver/path_lua.db", "P=$(PREFIX), PORT=apd_port, ADDR=0")
+dbLoadRecords("examples/asynPortDriver/process_chain.db", "P=$(PREFIX), PORT=apd_port, ADDR=0")
