11ba3e90775b1ea335d47ae55879cfd3741247fd
nmarks99 <nmarks@anl.gov>
2024-10-01 15:25:54 -0500

    add gen_display_path, update get_motor_records.py

---
+++ tools/gen_display_path/README.md
@@ -0,0 +1,4 @@
+- Same as the `setup_epics_common` script included with xxx, but outputs the full
+caQtDM or MEDM display path to a text file
+- Before using, define synApps support path and include its RELEASE file in configure/RELEASE
+
--- tools/get_motor_records.py
+++ tools/get_motor_records.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 from epics import caget
+import argparse
 
 # TODO: make args with argparse
 #  prefix = "4ida:"
 #  motor_list = list(range(1,13+1))
 
-prefix = "7idasoft:"
-motor_list = list(range(1,15+1))
 
 basic_fields = [
     "DESC",
@@ -166,8 +165,34 @@ all_fields = [
 
 def main():
 
+    parser = argparse.ArgumentParser(description='Get fields of motor records')
+    parser.add_argument('prefix', type=str, help='IOC name')
+    parser.add_argument('motors', type=str, nargs='+', help='Motor numbers to get')
+    parser.add_argument('--basic', action='store_true', help='Optional flag to enable basic mode')
+    parser.add_argument('--outfile', type=str, help='Path to an output file')
+    args = parser.parse_args()
+    
+    prefix = args.prefix
+    if ":" not in prefix:
+        prefix = f"{prefix}:"
+   
+    motor_list = []
+    if len(args.motors) == 1:
+        assert "-" in args.motors[0]
+        motors_split = args.motors[0].split("-")
+        m_start = motors_split[0].strip()
+        m_end = motors_split[1].strip()
+        motor_list = list(range(int(m_start),int(m_end)+1))
+    else:
+        motor_list = [int(m) for m in args.motors]
+
     motors = []
-    fields = basic_fields
+    fields = all_fields
+    if args.basic:
+        fields = basic_fields
+    
+    outfile = f"{prefix.replace(':','')}_all_motors.txt"
+    outfile = args.outfile if args.outfile else outfile
     
     for i in motor_list:
         pv_name = f"{prefix}m{i}"
@@ -176,18 +201,21 @@ def main():
         _motor = dict()
         _motor["PV_NAME"] = pv_name
         for field in fields:
+            print(f"{field}")
             val = caget(f"{pv_name}.{field}")
             _motor[field] = val
         motors.append(_motor)
 
     # save to text files
-    with open(f"{prefix.replace(':','')}_all_motors.txt", "w") as file:
+    with open(outfile, "w") as file:
         for motor in motors:
             pv_name = motor["PV_NAME"]
             for k, v in motor.items():
                 if k != "PV_NAME":
                     file.write(f"{pv_name}.{k} {v}\n")
+            file.write("\n")
 
+    print(f"Motor record fields saved to {outfile}")
 if __name__ == "__main__":
     main()
 
